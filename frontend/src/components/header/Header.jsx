d0446421bf1ff25ba491904feb7c8716db6a8dfc
Jonathan Scattolini <jscattolini@gmail.com>
2022-10-27 14:58:51 +0200

    Update add search button in desktop responsive ok

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import alien from "@assets/header/alien.jpg"
-import dune from "@assets/header/dune.jpg"
-import future from "@assets/header/future.jpg"
-import interstellar from "@assets/header/interstellar.jpg"
-import matrix from "@assets/header/matrix.jpg"
-import terminator from "@assets/header/terminator.jpg"
-
+import React, { useState, useEffect } from "react";
+import alien from "@assets/header/alien.jpg";
+import dune from "@assets/header/dune.jpg";
+import future from "@assets/header/future.jpg";
+import interstellar from "@assets/header/interstellar.jpg";
+import matrix from "@assets/header/matrix.jpg";
+import terminator from "@assets/header/terminator.jpg";
 
 const sliderData = [
   {
@@ -30,29 +28,22 @@ const sliderData = [
 ];
 
 function Header() {
-
-
   const [currentSlide, setCurrentSlide] = useState(0);
-  //const [backgroundImage, setBackgroundImage] = useState(6);
+
   const slideLength = sliderData.length;
 
   const autoScroll = true;
   let slideInterval;
-  let intervalTime = 6000;
+  const intervalTime = 6000;
 
   const nextSlide = () => {
     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
-    //setBackgroundImage(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
-  };
 
-  const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
   };
 
   function auto() {
     slideInterval = setInterval(nextSlide, intervalTime);
   }
-
 
   useEffect(() => {
     if (autoScroll) {
@@ -61,33 +52,28 @@ function Header() {
     return () => clearInterval(slideInterval);
   }, [currentSlide]);
 
-
   return (
-
     <header className="header">
-
       <div className="slider">
-
         {sliderData.map((slide, index) => {
+          const id = `caroussel-${index}`;
           return (
             <div
               className={index === currentSlide ? "slide current" : "slide"}
-              key={index}
+              key={id}
             >
               {index === currentSlide && (
-
                 <img src={slide.image} alt="slide" className="image" />
-
-
               )}
             </div>
           );
         })}
       </div>
       <div className="smartBox">
-
         <div className="wildMovies">
-          <h1>Wild<span>Movies</span></h1>
+          <h1>
+            Wild<span>Movies</span>
+          </h1>
         </div>
 
         <div>
@@ -95,16 +81,20 @@ function Header() {
         </div>
 
         <div>
-          <button className="searchButton">RECHERCHER</button>
+          <button type="button" className="searchButton">
+            RECHERCHER
+          </button>
         </div>
 
         <div className="searchBarBox">
-          <input className="searchBar" placeholder="Commencer ma recherche"></input>
+          <input className="searchBar" placeholder="Commencer ma recherche" />
+          <button type="button" className="searchButtonDesktop">
+            RECHERCHER
+          </button>
         </div>
-
       </div>
-    </header >
+    </header>
   );
-};
+}
 
 export default Header;
