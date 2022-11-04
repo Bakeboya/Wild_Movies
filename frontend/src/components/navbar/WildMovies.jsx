59a697d6fcc997823fe961a71777eef873573841
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-11-04 10:32:56 +0100

    Working search bar and return home after search

--- frontend/src/components/navbar/WildMovies.jsx
+++ frontend/src/components/navbar/WildMovies.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo_wildmovies.svg";
 import title from "../../assets/logo3_wildmovies.svg";
 
-function WildMovies() {
+function WildMovies({ setResults }) {
   const upToGo = () => {
     window.scrollTo(0, 0);
+    setResults([]);
   };
 
   return (
@@ -19,3 +21,7 @@ function WildMovies() {
 }
 
 export default WildMovies;
+
+WildMovies.propTypes = {
+  setResults: PropTypes.func.isRequired,
+};
