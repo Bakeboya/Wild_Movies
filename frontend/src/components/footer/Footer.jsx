506891f557cf9c50ad3058b3186e68df67be3f3f
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-11-17 16:15:30 +0100

    Worked on the footer - created button to go to top

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,37 +3,41 @@ import logoWildmovies from "../../assets/logo footer/wildmovies.svg";
 import BilFacebook from '@meronex/icons/bi/BilFacebook';
 import BilTwitter from '@meronex/icons/bi/BilTwitter';
 import BilInstagramAlt from '@meronex/icons/bi/BilInstagramAlt';
+import FiArrowUpCircle from '@meronex/icons/fi/FiArrowUpCircle';
 
 function Footer() {
   return (
     <footer className="footerApp">
-      <img src={logoWildmovies} alt="logoW" id="logoW" />
-      <div className="textLogo">
-        <div className="logos">
-          <a
-            href="https://www.facebook.com/profile.php?id=100086913807582"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BilFacebook />
-          </a>
-          <a
-            href="https://twitter.com/Wild_M0vies"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BilTwitter />
-          </a>
-          <a
-            href="https://www.instagram.com/wild_m0vies/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BilInstagramAlt />
-          </a>
+      <div className="footerContainer">
+        <img src={logoWildmovies} alt="logoW" id="logoW" />
+        <div className="textLogo">
+          <div className="logos">
+            <a
+              href="https://www.facebook.com/profile.php?id=100086913807582"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BilFacebook />
+            </a>
+            <a
+              href="https://twitter.com/Wild_M0vies"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BilTwitter />
+            </a>
+            <a
+              href="https://www.instagram.com/wild_m0vies/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BilInstagramAlt />
+            </a>
+          </div>
         </div>
-        <p className="text">© Team Movies. All rights reserved.</p>
+        <button className="btnToTop" onClick={() => { window.scrollTo(0, 0) }}><FiArrowUpCircle /></button>
       </div>
+      <p className="text">© Team Movies. All rights reserved.</p>
     </footer>
   );
 }
