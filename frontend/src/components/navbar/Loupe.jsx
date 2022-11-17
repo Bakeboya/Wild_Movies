8635f11a596080732ca4f4f48d0f9b95d18890c2
vincentRouget <vincent.rouget86@gmail.com>
2022-11-17 11:31:58 +0100

    Etat connecté fonctionnel

--- frontend/src/components/navbar/Loupe.jsx
+++ frontend/src/components/navbar/Loupe.jsx
@@ -1,48 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
 import GoSearch from "@meronex/icons/go/GoSearch";
+import { Link } from "react-router-dom";
 
-function Loupe({
-  setEmptySearch,
-  setSearch,
-  setSearchText,
-  setSearchPage,
-  setFiltersPage,
-  showModal,
-  setShowModal,
-  setShowSearchModal,
-  setShowFiltersModal,
-}) {
-  const openCloseModal = () => {
-    setShowModal(!showModal);
-    setSearch("");
-    setSearchText("");
-    setSearchPage(1);
-    setFiltersPage(1);
-    setShowSearchModal(false);
-    setShowFiltersModal(false);
-    setEmptySearch("");
-  };
-
+function Loupe() {
   return (
     <div className="LoupeDiv">
-      <button type="button" className="Loupe" onClick={openCloseModal}>
+      <Link to="/search" className="Loupe">
         <GoSearch className="GoSearch" />
-      </button>
+      </Link>
     </div>
   );
 }
 
 export default Loupe;
-
-Loupe.propTypes = {
-  setEmptySearch: PropTypes.func.isRequired,
-  setSearch: PropTypes.func.isRequired,
-  setSearchText: PropTypes.func.isRequired,
-  setSearchPage: PropTypes.func.isRequired,
-  setFiltersPage: PropTypes.func.isRequired,
-  showModal: PropTypes.bool.isRequired,
-  setShowModal: PropTypes.func.isRequired,
-  setShowSearchModal: PropTypes.func.isRequired,
-  setShowFiltersModal: PropTypes.func.isRequired,
-};
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,68 +1,33 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
 import UserIcon from "@components/navbar/UserIcon";
+import PropTypes from "prop-types";
 import Loupe from "./Loupe";
 import WildMovies from "./WildMovies";
 
-function Navbar({
-  setEmptySearch,
-  setResults,
-  setSearch,
-  setSearchText,
-  setSearchPage,
-  setFiltersPage,
-  showModal,
-  setShowModal,
-  setShowSearchModal,
-  setShowFiltersModal,
-}) {
+function Navbar({ setResults }) {
   const [fixed, setFixed] = useState(true);
-  // const [connected, setConnected] = useState(false);
-
-  const detailConnected = () => {
-    // console.log(connected);
-  };
+  const [connected, setConnected] = useState(false);
+  const [email, setEmail] = useState("");
 
   return (
     <nav className={!fixed ? "Navbar" : "Navbar Bottom"}>
-      <WildMovies
-        setEmptySearch={setEmptySearch}
-        setSearch={setSearch}
-        setSearchText={setSearchText}
-        setSearchPage={setSearchPage}
-        setFiltersPage={setFiltersPage}
-        setResults={setResults}
-        setShowModal={setShowModal}
-        setShowSearchModal={setShowSearchModal}
-        setShowFiltersModal={setShowFiltersModal}
-      />
-      <Loupe
-        setEmptySearch={setEmptySearch}
-        setSearch={setSearch}
-        setSearchText={setSearchText}
-        setSearchPage={setSearchPage}
-        setFiltersPage={setFiltersPage}
-        showModal={showModal}
-        setShowModal={setShowModal}
-        setShowSearchModal={setShowSearchModal}
-        setShowFiltersModal={setShowFiltersModal}
-      />
-      <UserIcon />
+      <WildMovies setResults={setResults} />
+      <Loupe />
+      <div className="LoginDiv">
+        <div className="Login">
+          {connected && <p>{email}</p>}
+          <UserIcon
+            setEmail={setEmail}
+            email={email}
+            setConnected={setConnected}
+            connected={connected}
+          />
+        </div>
+      </div>
     </nav>
   );
 }
-
-export default Navbar;
-
 Navbar.propTypes = {
   setResults: PropTypes.func.isRequired,
-  setEmptySearch: PropTypes.func.isRequired,
-  setSearch: PropTypes.func.isRequired,
-  setSearchText: PropTypes.func.isRequired,
-  setSearchPage: PropTypes.func.isRequired,
-  setFiltersPage: PropTypes.func.isRequired,
-  showModal: PropTypes.bool.isRequired,
-  setShowModal: PropTypes.func.isRequired,
-  setShowSearchModal: PropTypes.func.isRequired,
-  setShowFiltersModal: PropTypes.func.isRequired,
 };
+export default Navbar;
