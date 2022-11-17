bca29e190e33d74295859122e12e3e596348ca02
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-11-17 12:41:28 +0100

    Bug correction

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,15 +4,46 @@ import PropTypes from "prop-types";
 import Loupe from "./Loupe";
 import WildMovies from "./WildMovies";
 
-function Navbar({ setResults }) {
+function Navbar({
+  setEmptySearch,
+  setResults,
+  setSearch,
+  setSearchText,
+  setSearchPage,
+  setFiltersPage,
+  showModal,
+  setShowModal,
+  setShowSearchModal,
+  setShowFiltersModal,
+}) {
   const [fixed, setFixed] = useState(true);
   const [connected, setConnected] = useState(false);
   const [email, setEmail] = useState("");
 
   return (
     <nav className={!fixed ? "Navbar" : "Navbar Bottom"}>
-      <WildMovies setResults={setResults} />
-      <Loupe />
+      <WildMovies
+        setEmptySearch={setEmptySearch}
+        setSearch={setSearch}
+        setSearchText={setSearchText}
+        setSearchPage={setSearchPage}
+        setFiltersPage={setFiltersPage}
+        setResults={setResults}
+        setShowModal={setShowModal}
+        setShowSearchModal={setShowSearchModal}
+        setShowFiltersModal={setShowFiltersModal}
+      />
+      <Loupe
+        setEmptySearch={setEmptySearch}
+        setSearch={setSearch}
+        setSearchText={setSearchText}
+        setSearchPage={setSearchPage}
+        setFiltersPage={setFiltersPage}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        setShowSearchModal={setShowSearchModal}
+        setShowFiltersModal={setShowFiltersModal}
+      />
       <div className="LoginDiv">
         <div className="Login">
           {connected && <p>{email}</p>}
@@ -27,7 +58,18 @@ function Navbar({ setResults }) {
     </nav>
   );
 }
+
+export default Navbar;
+
 Navbar.propTypes = {
   setResults: PropTypes.func.isRequired,
+  setEmptySearch: PropTypes.func.isRequired,
+  setSearch: PropTypes.func.isRequired,
+  setSearchText: PropTypes.func.isRequired,
+  setSearchPage: PropTypes.func.isRequired,
+  setFiltersPage: PropTypes.func.isRequired,
+  showModal: PropTypes.bool.isRequired,
+  setShowModal: PropTypes.func.isRequired,
+  setShowSearchModal: PropTypes.func.isRequired,
+  setShowFiltersModal: PropTypes.func.isRequired,
 };
-export default Navbar;
