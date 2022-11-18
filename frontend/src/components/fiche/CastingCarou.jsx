5a3b4b41730d6453fb2d99ba795fdc4b3d8867f5
GuiPich <110974962+GuiPich@users.noreply.github.com>
2022-11-18 16:37:11 +0100

    bug fix du carrousel actor

--- frontend/src/components/fiche/CastingCarou.jsx
+++ frontend/src/components/fiche/CastingCarou.jsx
@@ -5,53 +5,53 @@ import "react-multi-carousel/lib/styles.css";
 import CastingCarouCard from "./CastingCarouCard";
 
 const responsive = {
-  desktop: {
-    breakpoint: { max: 4000, min: 900 },
-    items: 4,
-    slidesToSlide: 4,
-  },
-  tablet: {
-    breakpoint: { max: 900, min: 600 },
-    items: 2,
-    slidesToSlide: 2,
-  },
-  mobile: {
-    breakpoint: { max: 600, min: 0 },
-    items: 1,
-    slidesToSlide: 1,
-  },
+    desktop: {
+        breakpoint: { max: 4000, min: 1024 },
+        items: 4,
+        slidesToSlide: 4,
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 430 },
+        items: 2,
+        slidesToSlide: 2,
+    },
+    mobile: {
+        breakpoint: { max: 430, min: 0 },
+        items: 1,
+        slidesToSlide: 1,
+    },
 };
 
 function CastingCarou({ cast, link }) {
-  return (
-    <div className="castingCarou">
-      <Carousel
-        containerClass="castingCarou_carousel"
-        responsive={responsive}
-        infinite
-      >
-        {cast &&
-          cast.map((infos) => {
-            return (
-              <div key={infos.id}>
-                <CastingCarouCard
-                  movieTitle={infos.name}
-                  movieImg={infos.profile_path}
-                  link={link}
-                  id={infos.id}
-                  job={infos.character || infos.job}
-                />
-              </div>
-            );
-          })}
-      </Carousel>
-    </div>
-  );
+    return (
+        <div className="castingCarou">
+            <Carousel
+                containerClass="castingCarou_carousel"
+                responsive={responsive}
+                infinite
+            >
+                {cast &&
+                    cast.map((infos) => {
+                        return (
+                            <div key={infos.id}>
+                                <CastingCarouCard
+                                    movieTitle={infos.name}
+                                    movieImg={infos.profile_path}
+                                    link={link}
+                                    id={infos.id}
+                                    job={infos.character || infos.job}
+                                />
+                            </div>
+                        );
+                    })}
+            </Carousel>
+        </div>
+    );
 }
 
 CastingCarou.propTypes = {
-  link: PropTypes.string.isRequired,
-  cast: PropTypes.shape(PropTypes.number, PropTypes.string).isRequired,
+    link: PropTypes.string.isRequired,
+    cast: PropTypes.shape(PropTypes.number, PropTypes.string).isRequired,
 };
 
 export default CastingCarou;
--- frontend/src/components/fiche/CastingCarouCard.jsx
+++ frontend/src/components/fiche/CastingCarouCard.jsx
@@ -3,42 +3,40 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 function CastingCarouCard({ movieImg, movieTitle, job, link, id }) {
+  return (
+    <div className="castingCarouCard">
+      <div className="castinCarouImg">
+        <Link to={link + id}>
+          {movieImg !== null ? (
+            <img
+              alt="Affiche du film"
+              className="castingCarouCard_img"
+              src={`https://image.tmdb.org/t/p/w200/${movieImg}`}
+            />
+          ) : (
+            <img
+              alt="Affiche du film non disponible"
+              className="castingCarouCard_img"
+              src={ImgTemp}
+            />
+          )}
+        </Link>
+      </div>
 
-
-    return (
-        <div className="castingCarouCard">
-            <div classname="castinCarouImg">
-                <Link to={link + id}>
-                    {movieImg !== null ? (
-                        <img
-                            alt="Affiche du film"
-                            className="castingCarouCard_img"
-                            src={`https://image.tmdb.org/t/p/w200/${movieImg}`}
-                        />
-                    ) : (
-                        <img
-                            alt="Affiche du film non disponible"
-                            className="castingCarouCard_img"
-                            src={ImgTemp}
-                        />
-                    )}
-                </Link>
-            </div>
-
-            <div className="castingCarouCard_title">
-                <p className="castingCarouCard_title_name">{movieTitle}</p>
-                <p className="castingCarouCard_title_year">{job}</p>
-            </div>
-        </div>
-    );
+      <div className="castingCarouCard_title">
+        <p className="castingCarouCard_title_name">{movieTitle}</p>
+        <p className="castingCarouCard_title_year">{job}</p>
+      </div>
+    </div>
+  );
 }
 
 CastingCarouCard.propTypes = {
-    movieImg: PropTypes.isRequired,
-    movieTitle: PropTypes.isRequired,
-    date: PropTypes.isRequired,
-    id: PropTypes.isRequired,
-    type: PropTypes.isRequired,
+  movieImg: PropTypes.string.isRequired,
+  movieTitle: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  job: PropTypes.string.isRequired,
 };
 
 export default CastingCarouCard;
